8f2658e97c239892c8b66ca50c2b23ba312a4068
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-03-17 09:11:04 -0400

    Add files via upload

--- business/views.py
+++ business/views.py
@@ -1,60 +1,42 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Package, Shipment
+from django.shortcuts import render, redirect
+from customer.models import Admin
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, redirect
+from customer.models import Admin
+from django.contrib import messages
+
+
+
+@login_required
+def business_portal(request):
+    user_type = request.session.get('user_type')
+    if user_type == 'admin':
+        try:
+            admin = Admin.objects.get(user_id=request.user.id)
+            return render(request, 'business/business_portal.html')
+        except Admin.DoesNotExist:
+            messages.error(request, 'This account is not associated with an admin.')
+            return redirect('customer-login')
+    else:
+        messages.error(request, 'You are not authorized to access the business portal.')
+        return redirect('customer-dashboard')
+
 
 def home(request):
   return render(request, 'home.html') 
 
 def business_portal(request):
-    return render(request, 'business/business_portal.html')
-
+    try:
+        admin = Admin.objects.get(user_id=request.user.id)
+        return render(request, 'business/business_portal.html')
+    except Admin.DoesNotExist:
+        return redirect('customer-login')
 
 def inventory_view(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        packages = Package.objects.filter(customer=customer)
-        shipments = Shipment.objects.filter(customer=customer).distinct()
-    else:
-        packages = []
-        shipments = []
-
+    packages = Package.objects.all()
     context = {
-        'packages': packages,
-        'shipments': shipments,
-        'customer': customer,
+        'packages': packages 
     }
     return render(request, 'business/business_portal.html', context)
-
-
-def package_detail(request, package_id):
-    package = get_object_or_404(Package, package_id=package_id)
-    context = {
-        'package': package,
-    }
-    return render(request, 'business/package_detail.html', context)
-
-
-def dispatch_package(request, package_id):
-    package = get_object_or_404(Package, package_id=package_id)
-    # Add logic to dispatch the package to the queue in the Routes tab
-    messages.success(request, f'Package {package.package_id} has been dispatched.')
-    return redirect('business_portal')
-
-def deny_package(request, package_id):
-    package = get_object_or_404(Package, package_id=package_id)
-    # Add logic to deny the package and notify the customer
-    messages.success(request, f'Package {package.package_id} has been denied.')
-    return redirect('business_portal')
-
-def dispatch_shipment(request, shipment_id):
-    shipment = get_object_or_404(Shipment, id=shipment_id)
-    # Add logic to dispatch the shipment to the queue in the Routes tab
-    messages.success(request, f'Shipment {shipment.order_number} has been dispatched.')
-    return redirect('business_portal')
-
-def deny_shipment(request, shipment_id):
-    shipment = get_object_or_404(Shipment, id=shipment_id)
-    # Add logic to deny the shipment and notify the customer
-    messages.success(request, f'Shipment {shipment.order_number} has been denied.')
-    return redirect('business_portal')
